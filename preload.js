d09dfdeddc2c14f9686ef1d9e7e763d06d2f190a
Manuel <manuel.pelzer@yahoo.de>
2025-07-26 20:34:44 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,6 +4,5 @@ contextBridge.exposeInMainWorld('api', {
   startDownload: (args) => ipcRenderer.invoke('start-download', args),
   onLog: (callback) => ipcRenderer.on('log', (_, msg) => callback(msg)),
   selectFolder: () => ipcRenderer.invoke('select-folder'),
-  onProgressUpdate: (callback) => ipcRenderer.on('progress', (event, data) => callback(data)),
   abortCrawling: () => ipcRenderer.invoke('abort-crawling')
 });
